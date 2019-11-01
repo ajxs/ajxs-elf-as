46c0fb2e381821e6f3324887c2a304239c9f2024
Anthony <ajxs@panoptic.online>
2019-11-02 00:58:05 +1100

    Testing improvements

--- src/test/arch/mips/codegen.c
+++ src/test/arch/mips/codegen.c
@@ -8,7 +8,7 @@
 #include <section.h>
 #include <stdlib.h>
 #include <symtab.h>
-
+#include <test.h>
 
 int init_codegen_test_suite(void) {
 	return 0;
@@ -23,18 +23,24 @@ int teardown_codegen_test_suite(void) {
 void test_encode_r_type(void) {
 	Encoding_Entity* encoded_instruction = NULL;
 	Assembler_Status status;
+	uint8_t opcode = 0;
+	uint8_t func = 0;
+	uint8_t rd = 0;
+	uint8_t rs = 0;
+	uint8_t rt = 0;
 
 	// ADD, $t0, $t0, $t1
-	uint8_t opcode = 0;
-	uint8_t func = 0x20;
-	uint8_t rd = encode_operand_register(REGISTER_$T0);
-	uint8_t rs = encode_operand_register(REGISTER_$T0);
-	uint8_t rt = encode_operand_register(REGISTER_$T1);
+	opcode = 0;
+	func = 0x20;
+	rd = encode_operand_register(REGISTER_$T0);
+	rs = encode_operand_register(REGISTER_$T0);
+	rt = encode_operand_register(REGISTER_$T1);
 
 	status = encode_r_type(&encoded_instruction,
 		opcode, rd, rs, rt, 0, func);
 
 	CU_ASSERT(*(uint32_t*)encoded_instruction->data == 0x1094020);
+	CU_ASSERT(status == ASSEMBLER_STATUS_SUCCESS);
 
 	free_encoding_entity(encoded_instruction);
 
@@ -48,8 +54,8 @@ void test_encode_r_type(void) {
 	status = encode_r_type(&encoded_instruction,
 		opcode, rd, rs, rt, 0, func);
 
-
 	CU_ASSERT(*(uint32_t*)encoded_instruction->data == 0x1284822);
+	CU_ASSERT(status == ASSEMBLER_STATUS_SUCCESS);
 
 	free_encoding_entity(encoded_instruction);
 }
@@ -58,6 +64,13 @@ void test_encode_r_type(void) {
 void test_encode_i_type(void) {
 	/** The executable symbol table. */
 	Symbol_Table symbol_table;
+	uint8_t opcode = 0;
+	uint8_t rs = 0;
+	uint8_t rt = 0;
+	Operand imm;
+	size_t program_counter = 0;
+	Encoding_Entity* encoded_instruction = NULL;
+	Assembler_Status status;
 
 	// Initialise with room for the null symbol entry.
 	symbol_table.n_entries = 1;
@@ -73,20 +86,12 @@ void test_encode_i_type(void) {
 	symbol_table.symbols[0].name = malloc(1);
 	symbol_table.symbols[0].name[0] = '\0';
 
-
-	Encoding_Entity* encoded_instruction = NULL;
-	Assembler_Status status;
-
-	// ADDI $t0, $t0, 0x50
-	uint8_t opcode = 0x8;
-	uint8_t rs = encode_operand_register(REGISTER_$T0);
-	uint8_t rt = encode_operand_register(REGISTER_$T1);
-
-	Operand imm;
+	// ADDI $t1, $t0, 0x50
+	opcode = 0x8;
+	rs = encode_operand_register(REGISTER_$T0);
+	rt = encode_operand_register(REGISTER_$T1);
 	imm.type = OPERAND_TYPE_NUMERIC_LITERAL;
 	imm.numeric_literal = 0x50;
-
-	size_t program_counter = 0;
 
 	status = encode_i_type(&encoded_instruction,
 		&symbol_table,
@@ -96,9 +101,26 @@ void test_encode_i_type(void) {
 		imm,
 		program_counter);
 
-	printf("::::0x%x\n", *(uint32_t*)encoded_instruction->data);
+	CU_ASSERT(*(uint32_t*)encoded_instruction->data == 0x21090050);
+	CU_ASSERT(status == ASSEMBLER_STATUS_SUCCESS);
 
-	CU_ASSERT(*(uint32_t*)encoded_instruction->data == 0x21280050);
+	// ORI $t3, $a0, 0x50
+	opcode = 13;
+	rs = encode_operand_register(REGISTER_$A0);
+	rt = encode_operand_register(REGISTER_$T3);
+	imm.type = OPERAND_TYPE_NUMERIC_LITERAL;
+	imm.numeric_literal = 0x50;
+
+	status = encode_i_type(&encoded_instruction,
+		&symbol_table,
+		opcode,
+		rs,
+		rt,
+		imm,
+		program_counter);
+
+	CU_ASSERT(*(uint32_t*)encoded_instruction->data == 0x348B0050);
+	CU_ASSERT(status == ASSEMBLER_STATUS_SUCCESS);
 
 	free_encoding_entity(encoded_instruction);
 
